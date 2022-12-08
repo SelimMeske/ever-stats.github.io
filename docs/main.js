d4569bbc8af78290b3c5a5179504ef4db19ed097
selim <mesic.selim@outlook.com>
2022-12-08 13:38:38 +0100

    Added logo

--- docs/main.js
+++ docs/main.js
@@ -11,12 +11,15 @@ response.then((response) => {
     for (let i = 0; i < response.length; i++) {
         let questionCount = list_reversed[i].questions.length;
         let questionName = list_reversed[i].name + " ";
+        let questionImageLink = list_reversed[i].logo;
+
         const para = document.createElement('p');
         const count = document.createElement('p');
         const image = document.createElement('img');
 
         para.className = "quizName";
         count.className = "count";
+        image.className = "logo";
         
         const container = document.createElement('div');
         container.id = "quizContainer";
@@ -24,13 +27,18 @@ response.then((response) => {
         para.innerHTML = questionName;
         count.innerHTML = questionCount;
 
+        image.src = questionImageLink;
+        image.width = 20;
+
         if (questionCount >= 50) {
             container.classList.add("countGood");
         }else {
             container.classList.add("countNotGood");
         }
-        container.appendChild(para)
-        container.appendChild(count)
+
+        container.appendChild(image);
+        container.appendChild(para);
+        container.appendChild(count);
 
         
         const main = document.getElementById('main');
